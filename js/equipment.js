2b3c599178fb3514789ad1d609aab61d66cdc235
TAKA-PHASE3 <heeze3160@gmail.com>
2026-06-08 16:13:43 +0800

    Update equipment.js

--- js/equipment.js
+++ js/equipment.js
@@ -1,21 +1,33 @@
+if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "index.html";
+}
+
 fetch("data/equipment.json")
 .then(response => response.json())
 .then(data => {
 
-let tbody =
-document.getElementById("equipmentBody");
+    let tbody =
+        document.getElementById("equipmentBody");
 
-data.forEach(item => {
+    data.forEach(item => {
 
-tbody.innerHTML += `
-<tr>
-<td>${item.id}</td>
-<td>${item.description}</td>
-<td>${item.qty}</td>
-<td>${item.status}</td>
-</tr>
-`;
+        tbody.innerHTML += `
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.description}</td>
+            <td>${item.qty}</td>
+            <td>${item.status}</td>
+        </tr>
+        `;
+
+    });
 
 });
 
-});
+function logout() {
+
+    localStorage.removeItem("loggedIn");
+
+    window.location.href = "index.html";
+
+}
