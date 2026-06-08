f1b1994aaf10c8f3690b1e5462bea9fdae5b3934
TAKA-PHASE3 <heeze3160@gmail.com>
2026-06-08 16:14:01 +0800

    Update submission.js

--- js/submission.js
+++ js/submission.js
@@ -1,20 +1,32 @@
+if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "index.html";
+}
+
 fetch("data/submission.json")
 .then(response => response.json())
 .then(data => {
 
-let tbody =
-document.getElementById("submissionBody");
+    let tbody =
+        document.getElementById("submissionBody");
 
-data.forEach(item => {
+    data.forEach(item => {
 
-tbody.innerHTML += `
-<tr>
-<td>${item.no}</td>
-<td>${item.title}</td>
-<td>${item.status}</td>
-</tr>
-`;
+        tbody.innerHTML += `
+        <tr>
+            <td>${item.no}</td>
+            <td>${item.title}</td>
+            <td>${item.status}</td>
+        </tr>
+        `;
+
+    });
 
 });
 
-});
+function logout() {
+
+    localStorage.removeItem("loggedIn");
+
+    window.location.href = "index.html";
+
+}
